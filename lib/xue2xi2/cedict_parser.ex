df4a3d7a44a6b3a353fc6b95078fc14ee1929b41
David Parry <david.parry@suranyami.com>
2018-03-17 14:33:57 +1100

    Now parses all dictionary entries

--- lib/xue2xi2/cedict_parser.ex
+++ lib/xue2xi2/cedict_parser.ex
@@ -1,7 +1,36 @@
-defmodule CEDictParser do
+defmodule Xue2xi2.CEDictParser do
+  alias Xue2xi2.{Entry, Repo}
+
   def parse(file) do
     File.stream!(file)
-    |> Stream.take(1)
+    |> Stream.map(fn line ->
+      with {:ok, entry} <- parse_line(line)
+      do
+        IO.puts entry.pinyin
+        entry
+      end
+    end)
     |> Enum.to_list()
+  end
+
+  def parse_line(line) when binary_part(line, 0, 1) == "#", do: nil
+  def parse_line(line) do
+    [_, start, descr] =
+      ~r/^(.+)\/(.+)\/$/Uui
+      |> Regex.run(line)
+
+    [_, chinese, pinyin] =
+      ~r/^(.+)\[(.+)\]/Uui
+      |> Regex.run(start)
+
+
+    [traditional, simplified | _] = String.split(chinese, " ")
+    %Entry{
+      traditional: traditional,
+      simplified: simplified,
+      pinyin: pinyin,
+      description: String.replace(descr, "/", "\n")
+    }
+    |> Repo.insert()
   end
 end
