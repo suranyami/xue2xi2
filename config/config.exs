d1de0f6112b32aa8b1098808cbbd6cd5cd5d97f6
David Parry <david.parry@suranyami.com>
2018-03-17 14:30:50 +1100

    Make the SQL logging less chatty

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,8 @@ config :xue2xi2, Xue2xi2Web.Endpoint,
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:user_id]
+  metadata: [:user_id],
+  level: :info
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
